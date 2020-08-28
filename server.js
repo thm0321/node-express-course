8e3e552cb40ec94a550daa5d68d7d80e4918e58c
Thomas Mendola <thomasmendola@Thomass-Air.home>
2020-08-28 13:31:55 -0400

    add first GET route

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
 
+const mockUserData = [
+    {name:'Mark'},
+    {name:'Jill'}
+]
+
 app.listen(8000, function(){
     console.log("server is running")
 })
