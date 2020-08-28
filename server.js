ebbc3a1cf0dfe66cb19e65b7fb39e597529700ef
Thomas Mendola <thomasmendola@Thomass-Air.home>
2020-08-28 14:06:42 -0400

    added dynamic GET route

--- server.js
+++ server.js
@@ -9,3 +9,20 @@ const mockUserData = [
 app.listen(8000, function(){
     console.log("server is running")
 })
+
+app.get('/users', function(req, res){
+    res.json({
+        success: true,
+        message: 'successfullt got users. Nice!',
+        users: mockUserData
+    })
+})
+
+app.get('/users/:id', function (req, res) {
+    let id = req.params.id
+    res.json({
+        success: true,
+        message: 'got one user!',
+        users: id
+    })
+})
